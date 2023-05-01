5e4414ad4ed971285a49e2cadb3c5f642ab30c9b
Ditidey <ditidey2017@gmail.com>
2023-05-01 16:18:49 +0100

    app get and listen added

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 5000;
+
+app.get('/', (req, res)=>{
+    res.send('Hello from server')
+})
+app.listen(port, ()=>{
+    console.log(`Server is running: ${port}`)
+})
