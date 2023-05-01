0e781a8f9d4757229cf593c1cedbe7d205f3bc71
Ditidey <ditidey2017@gmail.com>
2023-05-01 17:28:24 +0100

    vercel config added

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const app = express();
 const cors = require('cors');
 const port = process.env.port || 5000;
 
+const chefs = require('./data/chefs.json');
+
 app.use(cors());
-const chefs = require('./data/chefs.json')
+ 
 app.get('/', (req, res)=>{
     res.send('Hello from server')
 })
 
+app.get('/chefs', (req, res) =>{
+    res.send(chefs)
+})
+
+app.get('/chefs/:id', (req, res)=>{
+    const id = req.params.id
+    const chefsFind = chefs.find(chef => chef.id == id)
+    res.send(chefsFind)
+})
 app.listen(port, ()=>{
     console.log(`Server is running: ${port}`)
 })
