2fed274985d62f7ebde1647da3b1e68648124f43
Ditidey <ditidey2017@gmail.com>
2023-05-01 17:14:03 +0100

    json file added

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const cors = require('cors');
 const port = process.env.port || 5000;
 
 app.use(cors());
+const chefs = require('./data/chefs.json')
 app.get('/', (req, res)=>{
     res.send('Hello from server')
 })
+
 app.listen(port, ()=>{
     console.log(`Server is running: ${port}`)
 })
